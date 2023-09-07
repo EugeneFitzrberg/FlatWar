482f50d1b92b51d16b04f892aedddc321f2cd880
Eugene Yakovlev <116457504+EugeneYakovlevApp@users.noreply.github.com>
2023-09-07 21:51:32 +0500

    Second commit

--- flatwar/PlayerController.cs
+++ flatwar/PlayerController.cs
@@ -5,29 +5,42 @@ public class PlayerController : KinematicBody2D
 {
 	
 	private int speed = 200;
-
+	private AnimatedSprite mainSprite;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		mainSprite = (AnimatedSprite)GetNode("MainSprite");
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(float delta)
-  {   
+  {
 	Vector2 velocity = new Vector2();
-	if(Input.IsActionPressed("ui_left")){
-		velocity.x -= speed;
-	}
+		if (Input.IsActionPressed("ui_left"))
+		{
+			velocity.x -= speed;
+			mainSprite.FlipH = true;
+		}
 	if(Input.IsActionPressed("ui_right")){
 		velocity.x += speed;
-	}
+			mainSprite.FlipH = false;
+		}
 	if(Input.IsActionJustPressed("ui_down")){
 		velocity.y += speed;
 	}
 	if(Input.IsActionJustPressed("ui_up")){
 		velocity.y -= speed;
 	}
+
+		if (Input.IsActionJustPressed("ui_select")) {
+			mainSprite.Playing = true;
+			if (mainSprite.Frame == 1) {
+				mainSprite.Playing = false;
+			}
+		}
+
+
 	MoveAndSlide(velocity,Vector2.Up);
   }
 }
